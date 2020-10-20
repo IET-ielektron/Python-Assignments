966d791be1d7b3d236dc1d4cb50faedf1cacbc6e
IET-ielektron <tech@ielektron.com>
2020-10-20 13:54:07 +0530

    Remove duplicated directory

--- sulthan_sheriff_assignment/Question2/mysite/myapi/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-from .models import employee
-
-class employeeSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = employee
-        fields = ('employee_id', 'employee_name','manager_name')
