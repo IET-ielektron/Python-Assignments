932853449cadd04b6cac53e4f21f31c9d0841ce7
IET-ielektron <tech@ielektron.com>
2020-10-20 13:53:12 +0530

    Remove duplicated directory

--- Tarini_prasad_assignments/Question2/EmpMgrRelProj/taskapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-
-@admin.register(EmployeeData)
-class StudentAdmin(admin.ModelAdmin):
-    list_display = ('ename','designation','manager')
-
-admin.site.register(Designation)
